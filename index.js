a1667491302e8fa04d0ad14145deade4f3ed9e3f
jrodriguez0423 <111903824+jrodriguez0423@users.noreply.github.com>
2022-10-27 01:43:17 -0700

    better

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 const { Client, GatewayIntentBits, EmbedBuilder, PermissionsBitField, Permissions} = require('discord.js');
 
-const prefix = 'band ';
+const prefix = '>';
 
 const client = new Client ({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
 
 client.on("ready", () => {
     console.log("TedBot is online!")
-
-    client.user.setActivity('Ted Simulator', { type: "PLAYING" });
+    const status = "Ted Simulator";
+    client.user.setActivity(status, { type: 'PLAYING' });
 })
 
 client.on("messageCreate", (message) => {
-    if (!message.content.startsWith(prefix) || message.author.bot) return;
+    console.log(message);
+
+    if (message.author.bot) return;
 
     const args = message.content.slice(prefix.length).split(/ +/);
     const command = args.shift().toLowerCase();
@@ -19,51 +21,64 @@ client.on("messageCreate", (message) => {
     //message array
 
     const messageArray = message.content.split(" ");
-    const argument = messageArray.slice(1);
+    const arguments = messageArray.slice(1);
     const cmd = messageArray[0];
+
+    console.log(command, messageArray);
 
     //COMMANDS
 
 
+    const arr = message.content.split(" ");
 
+    arr.includes("") //boolean
 
-//test command
+    if (arr.includes("bruh")) //i hate bruh -> true
+    {
+        message.channel.send("bruh");
+    }
+    if (arr.includes("jojo")) 
+    {
+        message.channel.send("Jo. Jo. Are you here, Jo. Jo?")
+    }
+    if (arr.includes("crash"))
+    {
+        message.channel.send("How many chinks do you have?")
+    }
+    if (arr.includes("chink"))
+    {
+        message.channel.send("How many chinks do you have?")
+    }
+    if (arr.includes("tone"))
+    {
+        message.channel.send("Watch your intonation!!")
+    }
+    if (arr.includes("breakfast"))
+    {
+        message.channel.send("Do you guys not eat breakfast because you don’t want to, or because you wake up too late to eat?")
+    }
+    if (arr.includes("white"))
+    {
+        if (arr.includes("people"))
+        {
+            message.channel.send("SCARED WHITE MAN DISEASE!!")
+        }
+    }
+    if (arr.includes(">complain"))
+    {
+        message.channel.send("I’m done complaining. Actually no, I love complaining.");
+    }
+    if (arr.includes(">name"))
+    {
+        message.channel.send("Theodore Newton.")
+    }
+    if (arr.includes(">help"))
+    {
+        message.channel.send("Help yourself.")
+        message.channel.send("Just kidding -")
+        message.channel.send(">complain, >name")
+    }
 
-var tabInp = ['hate band', 'fuck band', 'whats your favorite food'];
-var tabAnsw = ['Unacceptable. I will be assigning more SmartMusic assignments.', 'Unacceptable.', 'My favorite Chinese food is Mongolian Beef'];
-var content = message.content.split(' ');
-
-if (tabHInp.indexOf(message.content) > -1  && message.isMentioned(client.user)){
-    var row = Math.floor(Math.random() * tabAnsw.length);
-    message.channel.sendMessage(tabAnsw[row]);
-}
-
-
-if(command === 'test') {
-    message.channel.send("TedBot is working!")
-}
-
-if(command === 'fuck') {
-    message.channel.send("Unacceptable.")
-}
-
-if(command === 'calendar') {
-    message.channel.send("https://calendar.google.com/calendar/embed?src=tnewton@moreaucatholic.org&ctz=America/Los_Angeles&gsessionid=OK&pli=1")
-}
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+});
 
 client.login("MTAzNDU5MDM1NjgzNjE4ODI5Mg.GqUTRG.ZsEOuLKFnrEgoapfyHhmCa9-1NCtlm-xx91K2k");
